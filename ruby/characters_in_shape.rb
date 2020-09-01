9b637cf63c6710b53f7176cbcbf40d8d95f9150d
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-09-01 18:59:49 -0500

    multi division and characters in shape

---
+++ ruby/characters_in_shape.rb
@@ -0,0 +1,47 @@
+# Characters in Shapes
+# Create a function that counts how many characters make up a rectangular shape. You will be given an array of strings.
+
+# Examples
+# count_characters([
+#   "###",
+#   "###",
+#   "###"
+# ]) ➞ 9
+
+# count_characters([
+#   "22222222",
+#   "22222222",
+# ]) ➞ 16
+
+# count_characters([
+#   "------------------"
+# ]) ➞ 18
+
+# count_characters([]) ➞ 0
+
+# count_characters(["", ""]) ➞ 0
+# Notes
+# Return 0 if given an empty array.
+
+def count_characters(arr)
+  count = 0
+  for i in arr
+    count += i.length
+  end
+  puts count
+end
+
+count_characters([
+  "###",
+  "###",
+  "###"
+])
+
+count_characters([
+  "------------------"
+]) 
+
+count_characters([
+  "22222222",
+  "22222222",
+])
