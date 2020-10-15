7e9136a3b84c0bc1bb60753e678fe58c69b210cd
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-10-14 20:54:01 -0500

    Return min string number between two given string numbers

---
+++ JavaScripts/Strings/smallerStringNumber.js
@@ -0,0 +1,17 @@
+// Smaller String Number
+// Create a function that returns the smaller number.
+function smallerNum(n1, n2){
+  let x = Math.min(n1, n2)
+  return x.toString()
+}
+// Examples
+console.log(smallerNum("21", "44"))// ➞ "21"
+
+console.log(smallerNum("1500", "1"))// ➞ "1"
+
+console.log(smallerNum("5", "5"))// ➞ "5"
+// Notes
+// Numbers will be represented as strings, and your output should also be a string.
+// If both numbers tie, return either number.
+// Numbers will be positive.
+// Bonus: See if you can do this without converting to integers.
