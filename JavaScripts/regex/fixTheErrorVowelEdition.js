256967e7eab273b2e875d6d0c31f1f31275884a7
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-10-17 23:09:04 -0500

    Remove all vowels from a string

---
+++ JavaScripts/regex/fixTheErrorVowelEdition.js
@@ -0,0 +1,15 @@
+// Fix the Error: Vowel Edition
+// Your friend is trying to write a function that removes all vowels from a string. They write:
+
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, "")
+}
+// However, it seems that it doesn't work? Fix your friend's code so that it actually does remove all vowels.
+
+// Examples
+console.log(removeVowels("candy"))// ➞ "cndy"
+
+console.log(removeVowels("hello"))// ➞ "hllo"
+// The "e" is removed, but the "o" is still there!
+
+console.log(removeVowels("apple"))// ➞ "pple"
