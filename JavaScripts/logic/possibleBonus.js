d740d41d6eef7630d73b938a650268a5f542dadd
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-11-22 15:41:10 -0600

    Possible bonus

---
+++ JavaScripts/logic/possibleBonus.js
@@ -0,0 +1,29 @@
+// Tile Teamwork Tactics
+
+// In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided dice. If you advance your piece onto the same tile as another player's piece, both of you earn a bonus.
+
+// Given you and your friend's tile number, create a function that returns if it's possible to earn a bonus when you roll the dice.
+// Examples
+
+function possibleBonus(a, b){
+  if (a >= b){
+    return false
+  }
+  if ((a + 6) >= b ){
+    return true;
+  }
+  else 
+    return false;
+}
+
+console.log(possibleBonus(3, 7))// ➞ true
+
+console.log(possibleBonus(1, 9))// ➞ false
+
+console.log(possibleBonus(5, 3))// ➞ false
+
+// Notes
+
+//     You cannot move backward (which is why example #3 doesn't work).
+//     If you are already on the same tile, return false, as you would be advancing away.
+//     Expect only positive integer inputs.
