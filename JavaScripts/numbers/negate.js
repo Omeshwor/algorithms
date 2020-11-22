18b5acaa552d22afd482dc5038275ae35d339e47
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-11-22 15:53:26 -0600

    Negate all numbers of an array

---
+++ JavaScripts/numbers/negate.js
@@ -0,0 +1,23 @@
+// Negate the Array of Numbers
+
+// Given an array of numbers, negate all elements contained within.
+
+//     Negating a positive value -+n will return -n, because all +'s are removed.
+//     Negating a negative value --n will return n, because the first - turns the second minus into a +.
+
+// Examples
+function negate(arr){
+  for (var i = 0; i < arr.length; i++){
+    arr[i] = -(arr[i])
+  }
+  return arr;
+}
+console.log(negate([1, 2, 3, 4]))// ➞ [-1, -2, -3, -4]
+
+console.log(negate([-1, 2, -3, 4]))// ➞ [1, -2, 3, -4]
+
+console.log(negate([]))// ➞ []
+
+// Notes
+
+// If you get an empty array, return an empty array: []
