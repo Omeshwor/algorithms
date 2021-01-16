36cba942f3b6c1a2081d17d768a70ada7181e4a2
Omeshwor <omeshworlalshrestha@yahoo.com>
2021-01-16 17:00:30 -0600

    GCD of two numbers

---
+++ ruby/Numbers/gcd_lmc_1.rb
@@ -0,0 +1,18 @@
+# GCD and LCM ( Part 1)
+
+# Create a function that takes two numbers as arguments and returns the Greatest Common Divisor (GCD) of the two numbers.
+# Examples
+
+def gcd(num1, num2)
+  num1.gcd(num2)
+end
+
+puts gcd(3, 5)  #➞ 1
+
+puts gcd(14, 28)  #➞ 14
+
+puts gcd(4, 18)  #➞ 2
+
+# Notes
+
+# The GCD is the highest number that can divide both arguments without a remainder.
