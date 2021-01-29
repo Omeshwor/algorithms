1d24b405781f1e3669079f67c9d8d007f7797b90
Omeshwor <omeshworlalshrestha@yahoo.com>
2021-01-29 11:56:09 -0600

    Get least common multiple between two numbers

---
+++ ruby/Numbers/gcd_lcm_2.rb
@@ -0,0 +1,20 @@
+# GCD and LCM (Part 2)
+
+# Create a function that takes two numbers as arguments and return the LCM of the two numbers.
+# Examples
+
+def lcm(num1, num2)
+  num1.lcm(num2)
+end
+
+puts lcm(3, 5) #➞ 15
+
+puts lcm(14, 28) #➞ 28
+
+puts lcm(4, 6) #➞ 12
+
+# Notes
+
+#     Don't forget to return the result.
+#     You may want to use the GCD function to make this a little easier.
+#     LCM stands for least common multiple, the smallest multiple of both integers.
